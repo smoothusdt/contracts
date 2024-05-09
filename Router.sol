6381ee134a065e5a3637ee8418b2ba81342838c2
Alexey Nebolsin <alexey.nebolsin.jr@gmail.com>
2024-05-10 04:48:20 +0500

    Allow customisable feeCollector address

--- Router.sol
+++ Router.sol
@@ -7,14 +7,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  * @title Routes Ananas payments
  * @dev Transfers tokens from the user to to the recipient + collects the fee.
  */
-contract AnanasRouter {
+contract SmoothRouter {
 
-    address public constant feeCollector = address(0xA49134Dfc6d89372E44b7e4369c025c6d535EeB0);
-    IERC20 public constant USDT = IERC20(0xa614f803B6FD780986A42c78Ec9c7f77e6DeD13C);
     mapping (address => uint256) public nonces;
 
-    function transfer(address from, address to, uint256 transferAmount, uint256 feeAmount, uint256 nonce, uint8 v, bytes32 r, bytes32 s) public {
-        bytes32 digest = keccak256(abi.encodePacked("Ananas", address(this), from, to, transferAmount, feeAmount, nonce));
+    function transfer(address tokenAddress, address from, address to, uint256 transferAmount, address feeCollector, uint256 feeAmount, uint256 nonce, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 digest = keccak256(abi.encodePacked("Smooth", address(this), tokenAddress, from, to, transferAmount, feeCollector, feeAmount, nonce));
         bytes32 prefixedDigest = keccak256(abi.encodePacked("\x19TRON Signed Message:\n32", digest));
         address signer = ecrecover(prefixedDigest, v, r, s);
         require(from == signer, "WRONG SIGNATURE");
@@ -22,10 +20,11 @@ contract AnanasRouter {
         require(nonces[from] == nonce, "WRONG NONCE");
         nonces[from] += 1;
         
-        USDT.transferFrom(from, to, transferAmount);
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(from, to, transferAmount);
         
         if (feeAmount > 0) {
-            USDT.transferFrom(from, feeCollector, feeAmount);
+            token.transferFrom(from, feeCollector, feeAmount);
         }
     }
 }
